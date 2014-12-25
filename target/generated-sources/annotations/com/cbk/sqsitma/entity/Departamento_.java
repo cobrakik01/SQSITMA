88b202dc0051e3736f6681dcd8f4eb8ea5d7eb8f
Federico <cobrakik01@gmail.com>
2014-12-24 22:04:21 -0600

    Se pueder generar reportes de las peticiones y de los usuarios existentes, esto solo disponible para los administradores.

--- target/generated-sources/annotations/com/cbk/sqsitma/entity/Departamento_.java
+++ target/generated-sources/annotations/com/cbk/sqsitma/entity/Departamento_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-20T18:13:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-21T16:36:29")
 @StaticMetamodel(Departamento.class)
 public class Departamento_ { 
 
--- target/generated-sources/annotations/com/cbk/sqsitma/entity/Usuario_.java
+++ target/generated-sources/annotations/com/cbk/sqsitma/entity/Usuario_.java
@@ -11,7 +11,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-20T18:13:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-21T16:36:29")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
